2aad09f7f6b08f359eccdc500ac3bb228796c63f
jennifertramsu <jennifer.tramsu@gmail.com>
2024-10-25 20:10:18 -0400

    edit setup

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from genomic_benchmarks.loc2seq import download_dataset
 from pathlib import Path
 
-DATA_PATH = Path('data')
-DATA_PATH.mkdir(exist_ok=True)
+PATHS = [Path('data'), Path('models'), Path('results')]
+for path in PATHS:
+    path.mkdir(exist_ok=True)
 
 # Downloading dataset human_ocr_ensembl
-download_dataset('human_ocr_ensembl', version = 0, dest_path = DATA_PATH / 'human_ocr_ensembl')
+if not (PATHS[0] / 'human_ocr_ensembl').exists():
+    download_dataset('human_ocr_ensembl', version = 0, dest_path = path[0] / 'human_ocr_ensembl')
